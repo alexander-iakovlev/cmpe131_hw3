80d5e7d96758b2f5aae497e491340cfb15a392e3
Alexander <alexander.iakovlev@sjsu.edu>
2022-03-28 23:21:52 -0700

    moved variable declaration

--- home.py
+++ home.py
@@ -3,9 +3,10 @@ from flask import escape
 
 myobj = Flask(__name__)
 
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
+
 @myobj.route("/")
 def home():
-    name = "Lisa"
-    city_names = ["Paris", "London", "Rome", "Tahiti"]
     return "<h1>Welcome " + name + "!</h1> <p><a href=\"https://www.google.com/\">not google</a></p> <ul> <li>"+city_names[0]+"</li><li>"+city_names[1]+"</li><li>"+city_names[2]+"</li><li>"+city_names[3]+"</li> </ul>"
 #myapp_obj.run()
