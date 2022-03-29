3ea076dc9864bf1444599365c9582414c04afa17
Alexander <alexander.iakovlev@sjsu.edu>
2022-03-28 23:11:49 -0700

    commented out run()

--- home.py
+++ home.py
@@ -8,4 +8,4 @@ def home():
     name = "Lisa"
     city_names = ["Paris", "London", "Rome", "Tahiti"]
     return "<h1>Welcome " + name + "!</h1> <p><a href=\"https://www.google.com/\">not google</a></p> <ul> <li>"+city_names[0]+"</li><li>"+city_names[1]+"</li><li>"+city_names[2]+"</li><li>"+city_names[3]+"</li> </ul>"
-myapp_obj.run()
+#myapp_obj.run()
